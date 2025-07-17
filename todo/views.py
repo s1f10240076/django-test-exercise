5a8d01b6b7fbb8c6c056cc9accc77bfab5cf1dd6
toby <tobyleung112@gmail.com>
2025-07-17 13:30:00 +0900

    edit.htmlの日本語バージョンを追加

--- todo/views.py
+++ todo/views.py
@@ -3,50 +3,87 @@ from django.http import Http404
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
+
 # Create your views here.
 
 
 def index(request):
-    if request.method == 'POST':
-        task = Task(title=request.POST['title'],
-                    due_at=make_aware(parse_datetime(request.POST['due_at'])))
+    if request.method == "POST":
+        task = Task(
+            title=request.POST["title"],
+            due_at=make_aware(parse_datetime(request.POST["due_at"])),
+        )
         task.save()
 
-    if request.GET.get('order') == 'due':
-        tasks = Task.objects.order_by('due_at')
+    if request.GET.get("order") == "due":
+        tasks = Task.objects.order_by("due_at")
     else:
-        tasks = Task.objects.order_by('-posted_at')
-    context = {
-        'tasks': tasks
-    }
-    return render(request, 'todo/index.html', context)
+        tasks = Task.objects.order_by("-posted_at")
+    context = {"tasks": tasks}
+    return render(request, "todo/index.html", context)
+
+
+def index_jp(request):
+    if request.method == "POST":
+        task = Task(
+            title=request.POST["title"],
+            due_at=make_aware(parse_datetime(request.POST["due_at"])),
+        )
+        task.save()
+
+    if request.GET.get("order") == "due":
+        tasks = Task.objects.order_by("due_at")
+    else:
+        tasks = Task.objects.order_by("-posted_at")
+    context = {"tasks": tasks}
+    return render(request, "todo/index_jp.html", context)
+
 
 def detail(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
-    
+
     context = {
-        'task': task,
+        "task": task,
     }
-    return render(request, 'todo/detail.html', context)
+    return render(request, "todo/detail.html", context)
+
 
 def update(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
-    if request.method == 'POST':
-        task.title = request.POST['title']
-        task.due_at = make_aware(parse_datetime(request.POST['due_at']))
+    if request.method == "POST":
+        task.title = request.POST["title"]
+        task.due_at = make_aware(parse_datetime(request.POST["due_at"]))
         task.save()
-        return redirect('detail', task_id=task_id)
+        return redirect("detail", task_id=task_id)
 
     context = {
-        'task': task,
+        "task": task,
     }
-    return render(request, 'todo/edit.html', context)
+    return render(request, "todo/edit.html", context)
+
+
+def update_jp(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    if request.method == "POST":
+        task.title = request.POST["title"]
+        task.due_at = make_aware(parse_datetime(request.POST["due_at"]))
+        task.save()
+        return redirect("detail", task_id=task_id)
+
+    context = {
+        "task": task,
+    }
+    return render(request, "todo/edit_jp.html", context)
+
 
 def close(request, task_id):
     try:
@@ -56,6 +93,7 @@ def close(request, task_id):
     task.completed = True
     task.save()
     return redirect(index)
+
 
 def delete(request, task_id):
     try:
